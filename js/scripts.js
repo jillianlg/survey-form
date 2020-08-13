d72020ee59c6597cf84d51a95033e425d79ed001
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-12 19:25:22 -0700

    update HTML input fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
+// Question 1 - Fill in boxes: Name
+// Question 2 - Date: When were you born 
+    // const dob = $("#born").val();
+// Question 3 - Select boxes: Italian, American, Sea Food, Thai, Greek, Soul Food, Japanese, Chinese, Mexican
+// Question 4 - Radio Buttons: Pop, Soul, R&B, Jazz, Country, Other
+// Question 5 - Color wheel: Favorite color
+
+
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
     // Locate and store value from input 1 (and log to console for testing purposes)
-    const answer1 = $("#question1").val();
-    console.log(answer1);
+    const name = $("input#name").val();
+    console.log(name);
     // Locate and store value from input 1
+    const dob = $("#born").val();
+    const cuisine = $("input#cuisine").val();
+    const music = $("input:radio[name=music]:checked").val();
+    const favoriteColor = $("input#color").val();
     // const answer2 = $("#question2").val();
   });
 });
 
-// Question 1 - Fill in boxes: Name
-// Question 2 - Date: When were you born 
-// Question 3 - Select boxes: Italian, American, Sea Food, Thai, Greek, Soul Food, Japanese, Chinese, Mexican
-// Question 4 - Radio Buttons: Pop, Soul, R&B, Jazz, Country, Other
-// Question 5 - Color wheel: Favorite color

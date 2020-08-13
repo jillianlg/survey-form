1f604b752f3a89e2fd1d08dd022a1c601d0e69dd
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-13 08:46:01 -0700

    fix .js input errors
    Co-Authored by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
-// Question 1 - Fill in boxes: Name
-// Question 2 - Date: When were you born 
-    // const dob = $("#born").val();
-// Question 3 - Select boxes: Italian, American, Sea Food, Thai, Greek, Soul Food, Japanese, Chinese, Mexican
-// Question 4 - Radio Buttons: Pop, Soul, R&B, Jazz, Country, Other
-// Question 5 - Color wheel: Favorite color
+// id="name" Question 1 - Fill in boxes: Name
+// id="dob" Question 2 - Date: When were you born 
+// id="cuisine" Question 3 - Select boxes: Italian, American, Sea Food, Thai, Greek, Soul Food, Japanese, Chinese, Mexican
+// id="music" Question 4 - Radio Buttons: Pop, Soul, R&B, Jazz, Country, Other
+// id="color" Question 5 - Color wheel: Favorite color
 
 
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
+        // // Locate and store value from input 1 (and log to console for testing purposes)
+    const nameInput = $("#name").val();
+        console.log(nameInput);
+    // // Locate and store value from input 1
+    const dobInput = $("#dob").val();
+        console.log(dobInput);
+    const cuisineInput = $("#cuisine").val();
+        console.log(cuisineInput);
+    const musicInput = $("input:radio[name=music]:checked").val();
+        console.log(musicInput);
+    const colorInput = $("#color").val();
+        console.log(colorInput);
+
+    $("#response").show();
+
     event.preventDefault();
-    // Locate and store value from input 1 (and log to console for testing purposes)
-    const name = $("input#name").val();
-    console.log(name);
-    // Locate and store value from input 1
-    const dob = $("#born").val();
-    const cuisine = $("input#cuisine").val();
-    const music = $("input:radio[name=music]:checked").val();
-    const favoriteColor = $("input#color").val();
-    // const answer2 = $("#question2").val();
   });
 });
 
